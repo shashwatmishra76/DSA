ad0bf347cff4dcaa256cefa5ceee693a09931468
Shashwat Mishra <shashwatmishra76@gmail.com>
2026-05-06 12:44:24 +0530

    Create 1878C.java

---
+++ Codeforces/CP-31/1878C.java
@@ -0,0 +1,86 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static FastScanner fs = new FastScanner(System.in);
+    static PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+    static final int MOD = 1_000_000_007;
+    static final int INF = (int) 1e9;
+
+    public static void main(String[] args) throws Exception {
+        int t = fs.nextInt();
+        //int t = 1;
+        
+        while (t-- > 0) {
+            solve();
+        }
+        
+        out.flush();
+    }
+
+    static void solve() {
+        long n, k, x;
+        n = fs.nextLong();
+        k = fs.nextLong();
+        x = fs.nextLong();
+        
+        long minPossible = 0, maxPossible = 0, totalSum;
+        
+        minPossible = (k * (k + 1)) / 2;
+        
+        maxPossible = ((n * (n + 1)) / 2) - ((n - k) * (n - k + 1) / 2); 
+        
+        if(x >= minPossible && x <= maxPossible) {
+            out.println("YES");
+        } else {
+            out.println("NO");
+        }
+        
+    }
+
+    // --------- Fast Scanner ----------
+    static class FastScanner {
+        private final BufferedReader br;
+        private StringTokenizer st;
+
+        FastScanner(InputStream is) {
+            br = new BufferedReader(new InputStreamReader(is));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    String line = br.readLine();
+                    if (line == null) return null;
+                    st = new StringTokenizer(line);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            try {
+                st = null;
+                return br.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
