52dd18ff762b87e28e615159cb16520187ddbd5a
Shashwat Mishra <shashwatmishra76@gmail.com>
2026-05-13 20:44:21 +0530

    Create Online_Stack_Span.java

---
+++ LeetCode/Stack/Online_Stack_Span.java
@@ -0,0 +1,26 @@
+class StockSpanner {
+    private Stack<int[]> st;
+
+    public StockSpanner() {
+        st = new Stack<>();
+    }
+    
+    public int next(int price) {
+        int span = 1;
+
+        while(!st.isEmpty() && st.peek()[0] <= price) {
+            int[] top = st.pop();
+            span += top[1];
+        }
+
+        st.push(new int[]{price, span});
+
+        return span;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
